76fd6aebecf16a235ad1e9669f8e71b58cc56926
Quentin Strinati <qstrinati@gmail.com>
2021-03-06 21:34:24 +0100

    Cinemachine camera follows player; Parallax Environment Scrolling; 2DCharacterController jump moves; Animations idle jump moves

---
+++ Assets/Scripts/Player2DController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+public class Player2DController : MonoBehaviour {
+
+    public RuntimeAnimatorController animatorController;
+    public float jumpMax = 3f;
+    public float jumpSpeed = 2f;
+    public float moveSpeed = 7f;
+    public float fallSpeed = 2f;
+    public GroundCheck groundCheck;
+    public bool isWalking;
+    public bool isIdle;
+    public bool isGrounded;
+    public bool isFlipped;
+    public bool jumps;
+    private float jumpLerp;
+    private float horizontalAxis;
+    private Rigidbody2D rigidBody2D;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        rigidBody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+        rigidBody2D.gravityScale = fallSpeed;
+        animator.runtimeAnimatorController = animatorController;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Flags and variables
+            //Variables
+        horizontalAxis = Input.GetAxisRaw("Horizontal");
+        jumps = Input.GetKeyDown(KeyCode.Space);
+            // Control Flags
+        isFlipped = horizontalAxis < 0;
+        isWalking = horizontalAxis != 0;
+        isIdle = horizontalAxis == 0;
+        isGrounded = groundCheck.isGrounded;
+
+        //Actions
+            //Jump
+        if (jumps && isGrounded) {
+            isGrounded = false;
+            rigidBody2D.AddForce(new Vector2(0, jumpMax), ForceMode2D.Impulse);
+        }
+        
+            //Movements
+        if (horizontalAxis != 0) {
+            spriteRenderer.flipX = isFlipped;
+            transform.position += new Vector3(horizontalAxis * moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        //Animations
+        if (!isGrounded) {
+            animator.SetBool("IsJumping", true);
+            animator.SetBool("IsWalking", false);
+        }
+        else if (isWalking) {
+            animator.SetBool("IsWalking", true);
+            animator.SetBool("IsJumping", false);
+        }
+        else if (isIdle) {
+            animator.SetBool("IsWalking", false);
+            animator.SetBool("IsJumping", false);}
+    }
+}
